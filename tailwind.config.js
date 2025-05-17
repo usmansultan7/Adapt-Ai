29e14aedb9a4847ca1ee3f5a659b2a51d835b922
usmansultan7 <h.usmansultan@gmail.com>
2025-05-18 03:30:00 +0500

    Adapt Ai website

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  // This must be present if you're using purge or safelisting
+  safelist: [
+    'backdrop-blur-2xl',
+    'bg-black/50'
+  ],
+}
